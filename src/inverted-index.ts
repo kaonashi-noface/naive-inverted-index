9b8ebb58c6c34586f36d583879055a72c30e2118
no_face <dev@paulckim.com>
2026-03-14 23:46:27 -0700

    add single term query scenario

--- src/inverted-index.ts
+++ src/inverted-index.ts
@@ -1,1 +1,19 @@
-// TODO: impl later...
+
+export class InvertedIndex {
+
+    dictionary: Map<string, number[]>;
+
+    constructor(dictionary: Map<string, number[]>) {
+        this.dictionary = dictionary;
+    }
+
+    query(term: string) : number[] {
+        if(!term || !this.dictionary.has(term)) {
+            return [];
+        }
+
+        const postings: number[] = this.dictionary.get(term)!;
+        return postings;
+    }
+
+}
--- tests/inverted-index.test.ts
+++ tests/inverted-index.test.ts
@@ -1,5 +1,18 @@
-describe('Some TestSuite', () => {
-    it('some test...', () => {
-        // TODO: impl later...
+import { InvertedIndex } from '@src/inverted-index';
+
+describe('Inverted Index TestSuite', () => {
+
+    const preprocessed = new Map<string, number[]>([
+        [ 'word1', [1, 3, 5, 89, 231] ],
+        [ 'word2', [2, 23, 52, 89] ],
+        [ 'word3', [2, 5, 23, 52] ],
+        [ 'word4', [23, 52, 123, 504] ],
+    ]);
+
+    it('should query for single word', () => {
+        const index = new InvertedIndex(preprocessed);
+        const actualResults = index.query('word3');
+        expect(actualResults).toStrictEqual([2, 5, 23, 52]);
     })
+
 })
