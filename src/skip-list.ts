aa17ca7dcea7cc46f86ba60d522356b2813b82c9
no_face <dev@paulckim.com>
2026-03-23 21:06:33 -0700

    reformat

--- src/skip-list.ts
+++ src/skip-list.ts
@@ -1,18 +1,17 @@
-
 export type SkipList = {
-    postings: number[],
-    skipPtrs: number[],
+    postings: number[];
+    skipPtrs: number[];
 };
 
-export function getSkipPointers(postings: number[]) : number[] {
+export function getSkipPointers(postings: number[]): number[] {
     const skipLength = Math.ceil(Math.sqrt(postings.length));
     const skipPtrs: number[] = getMultiples(postings.length, skipLength);
     return skipPtrs;
 }
 
-function getMultiples(n: number, k: number) : number[] {
+function getMultiples(n: number, k: number): number[] {
     const multiples: number[] = [];
-    for (let i=k; i<n; i+=k) {
+    for (let i = k; i < n; i += k) {
         multiples.push(i);
     }
     return multiples;
--- tests/skip-list.test.ts
+++ tests/skip-list.test.ts
@@ -4,7 +4,7 @@ describe('SkipList TestSuite', () => {
     it('should return skip pointers less than postings.length', () => {
         const postings = [1, 2, 4, 7, 27, 32, 56, 60, 62, 64, 68, 74];
         const skipPtrs = getSkipPointers(postings);
-        const lastSkipPtr = skipPtrs[skipPtrs.length-1];
+        const lastSkipPtr = skipPtrs[skipPtrs.length - 1];
         expect(lastSkipPtr).toBeLessThan(postings.length);
         expect(lastSkipPtr).toBe(8);
     });
@@ -12,7 +12,7 @@ describe('SkipList TestSuite', () => {
     it('should return skip pointers equal to postings.length', () => {
         const postings = [1, 2, 4, 7, 27, 32, 56, 60, 62, 64, 68, 74, 98];
         const skipPtrs = getSkipPointers(postings);
-        const lastSkipPtr = skipPtrs[skipPtrs.length-1];
+        const lastSkipPtr = skipPtrs[skipPtrs.length - 1];
         expect(lastSkipPtr).toBeLessThan(postings.length);
         expect(lastSkipPtr).toBe(12);
     });
@@ -20,7 +20,7 @@ describe('SkipList TestSuite', () => {
     it('should return skip pointers greater than postings.length', () => {
         const postings = [1, 2, 4, 7, 27, 32, 56, 60, 62, 64, 68, 74, 98, 100];
         const skipPtrs = getSkipPointers(postings);
-        const lastSkipPtr = skipPtrs[skipPtrs.length-1];
+        const lastSkipPtr = skipPtrs[skipPtrs.length - 1];
         expect(lastSkipPtr).toBeLessThan(postings.length);
         expect(lastSkipPtr).toBe(12);
     });
